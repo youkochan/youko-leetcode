11eac50fbadb536b294f773620521b69a38f88df
youkochan <youkochan233@gmail.com>
2016-01-19 17:30:42 +0800

    add 127.cpp

--- c/127.cpp
+++ c/127.cpp
@@ -1,46 +1,94 @@
+/**
+ * 127. Word Ladder
+ * https://leetcode.com/problems/word-ladder/
+ */
+
+#include<iostream>
+#include<unordered_set>
+#include<vector>
+
+using namespace std;
+
 class Solution {
 public:
-    bool check(string s1, string s2) {
-        int count = 0;
+    int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList) {
+        if (checkDiff(beginWord, endWord))
+            return 2;
+        int len = 2;
+        vector<string> temp1;
+        temp1.push_back(endWord);
+        vector<string> temp2;
+
+        while (true) {
+            /*
+            cout << "Temp1: ";
+            for (int i = 0; i < temp1.size(); i ++)
+                cout << temp1[i] << " ";
+            cout << endl;
+            */
+
+            unordered_set<string>::iterator it = wordList.begin();
+            for (it; it != wordList.end(); it ++) {
+                string s1 = *it;
+                for (int i = 0; i <= temp1.size() - 1; i ++) {
+                    string s2 = temp1[i];
+                    if (checkDiff(s1, s2)) {
+                        temp2.push_back(s1);
+                        break;
+                    }
+                }
+            }
+
+            /*
+            cout << "Temp2: ";
+            for (int i = 0; i < temp2.size(); i ++)
+                cout << temp2[i] << " ";
+            cout << "\n==========" << endl;
+            */
+
+            for (int i = 0; i < temp2.size(); i ++) {
+                if (checkDiff(temp2[i], beginWord))
+                    return len + 1;
+            }
+
+            if (temp2.size() == 0) {
+                return 0;
+            }
+
+            for (int i = 0; i <= temp2.size() - 1; i ++) {
+                wordList.erase(temp2[i]);
+            }
+            temp1 = temp2;
+            temp2.clear();
+            len ++;
+        }
+
+        return 0;
+    }
+
+    bool checkDiff(string &s1, string &s2) {
+        int diff = 0;
         for (int i = 0; i < s1.size(); i ++) {
             if (s1[i] != s2[i])
-                count ++;
+                diff ++;
+            if (diff > 1)
+                return false;
         }
-        if (count == 1)
+        if (diff == 1)
             return true;
         else
             return false;
     }
-
-    void search(string midWord, string &endWord, unordered_set<string>& wordList, 
-                unordered_set<string>& usedList, int step, int &ret) 
-    {
-        if (check(midWord, endWord)) {
-            if (step + 1 < ret)
-                ret = step + 1;
-            return;
-        }
-        unordered_set<string>::iterator it = wordList.begin();
-        for (; it != wordList.end(); it ++) {
-            if (usedList.find(*it) == usedList.end()) {
-                string word = *it;
-                if (check(word, midWord)) {
-                    usedList.insert(word);
-                    search(beginWord, endWord, wordList, usedList, step + 1, ret);
-                    usedList.erase(word);
-                }
-            }
-        }
-    }
-
-    int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList) {
-        if (beginWord == endWord)
-            return 1;
-        int ret = INT_MAX;
-        unordered_set<string> usedList;
-        search(beginWord, endWord, wordList, usedList, 1, ret);
-        if (ret == INT_MAX)
-            return 0;
-        return ret;
-    }
 };
+
+int main() {
+    Solution s;
+
+    string beginWord = "a";
+    string endWord = "c";
+    unordered_set<string> wordList = {"a","b","c"};
+
+    cout << s.ladderLength(beginWord, endWord, wordList);
+
+    return 0;
+}
