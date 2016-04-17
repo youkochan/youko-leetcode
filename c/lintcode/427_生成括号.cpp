f4feb27a06f80ecf2805b02dcea92646522cf946
youkochan <youkochan233@gmail.com>
2016-04-17 23:42:46 +0800

    add lintcode 427.cpp

--- c/lintcode/427_生成括号.cpp
+++ c/lintcode/427_生成括号.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
+    void find(int left, int right, string str, set<string> &mset) {
+        if (left == 0 && right == 0)
+            mset.insert(str);
+        if (left != 0)
+            find(left-1, right+1, str+'(', mset);
+        if (right != 0)
+            find(left, right-1, str+')', mset);
+    }
+
     /**
      * @param n n pairs
      * @return All combinations of well-formed parentheses
      */
     vector<string> generateParenthesis(int n) {
         // Write your code here
+        
         vector<string> result;
         if (n <= 0)
             return result;
-            
+        
         set<string> mset;
-        mset.insert("()");
+        find(n, 0, "", mset);
         
-        while (-- n) {
-            set<string> _mset;
-            set<string>::iterator it = mset.begin();
-            while (it != mset.end()) {
-                string _s = *it;
-                _mset.insert(_s + "()");
-                _mset.insert("()" + _s);
-                _mset.insert("(" + _s + ")");
-                it ++;
-            }
-            mset = _mset;
-        }
-        
-        set<string>::iterator it = mset.begin();
+        auto it = mset.begin();
         while (it != mset.end()) {
             result.push_back(*it);
             it ++;
